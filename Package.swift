f438dab6098919b4f0ad23082276381a505d3404
kapejod <kapejod@gmail.com>
2022-12-16 10:06:39 +0100

    Fix dependency to ahoywebrtc-swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ahoyconference/ahoywebrtc-swift.git", from: "1.3.0"),
+        .package(url: "https://github.com/ahoyconference/ahoywebrtc-swift.git", from: "1.5.0"),
     ],
     targets: [
         .binaryTarget(
