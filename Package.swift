72df5772a3fea262c29f41906793c70882ea82cf
Klaus-Peter Junghanns <kpj@ahoyrtc.com>
2026-07-17 08:59:46 +0200

    Update to AhoyWebRTC 1.10

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ahoyconference/ahoywebrtc-swift.git", from: "1.9.0"),
+        .package(url: "https://github.com/ahoyconference/ahoywebrtc-swift.git", from: "1.10.0"),
     ],
     targets: [
         .binaryTarget(
             name: "AhoyKit",
-            url: "https://demo.ahoyrtc.com/AhoyKit.xcframework.6.1.zip",
-            checksum: "c44ab815e41dc5c7e126d1605e99f6801213fc480771462efb33660988e3b855"
+            url: "https://demo.ahoyrtc.com/AhoyKit.xcframework.6.2.zip",
+            checksum: "f92b07405343036e6cb955f91c5a9e9af7a1929e6ea987548870586035756bc7"
         )
     ]
 )
